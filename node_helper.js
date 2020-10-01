3b8f36eae67da1a1d614f43b781522191ff2d563
lfaoanl <lfaoanl@gmail.com>
2020-10-01 20:42:41 +0200

    test

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ module.exports = NodeHelper.create({
     let self = this
     this.expressApp.post('/MMM-HomeAssistantPlaying', function (req, res) {
 //      self.sendSocketNotification("UPDATE_CURRENT_SONG");
-      res.status(200).send([req, req.body, req.params, req.query]);
+      res.status(200).send([req.body, req.params, req.query]);
     });
   },
 
